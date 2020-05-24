1eba8a1af30873f01890cbf9a7ae6c904104a78e
YaoLaiGang <2046682604@qq.com>
2020-05-24 22:00:19 +0800

    将用户输入改为文件输入，并直接从抓取的网页中抽取歌曲名称

--- down_music.py
+++ down_music.py
@@ -1,14 +1,17 @@
 import requests
 import re
+from tqdm import tqdm
+
+MUSIC_NAME = "./name.txt"
 
 re_music = re.compile(r'https\:\/\/music\.hwkxk\.cn\/api\/\?id\=[0-9]+\&source\=kuwoST')
+re_name = re.compile(r'下载.+?song-bitrate')
 def downMusic(music_name="不再打扰"):
     print("正在下载{}".format(music_name))
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
     url = 'https://music.hwkxk.cn/'
     cs = {'mk_encrypt_c21f969b5f03d33d43e04f8f136e7682':"bb5cf23bee8fc811f66fd85325589960"}
     rq = requests.get(url = url, headers = headers, cookies = cs, params={'kw': music_name, 'source': 'kuwo'})
-    print(rq.url)
     if rq.status_code==200:
         rq.encoding = "utf-8"
     else:
@@ -17,14 +20,20 @@ def downMusic(music_name="不再打扰"):
         exit(0)
     html_text = rq.text
     index = re_music.search(html_text).span()
+    name_index = re_name.search(html_text).span()
     music_url = html_text[index[0]:index[1]]
+    real_name = html_text[name_index[0]:name_index[1]][62:-28]
     music_rq = requests.get(url = music_url, headers = headers, cookies = cs)
     if music_rq.status_code!=200:
-        print("下载音乐{}失败".format(music_name))
+        print("下载音乐{}失败".format(real_name))
         exit(0)
-    with open(music_name+".mp3",'wb') as f:
+    with open(real_name+".mp3",'wb') as f:
         f.write(music_rq.content)
-        print("下载音乐{}成功".format(music_name))
+        print("下载音乐{}成功".format(real_name))
 
+name_file = open(MUSIC_NAME)
+music_list = name_file.readlines()
+dj_music = map(lambda x: x[:-1]+"DJ", music_list)
 
-downMusic("夜曲")
+for music_name in dj_music:
+    downMusic(music_name)
